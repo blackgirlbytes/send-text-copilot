865da75a77cb97290030e4914d1799989999c7c2
Rizel Scarlett <blackgirlbytes@github.com>
2022-10-11 16:04:32 +0000

    send text code

---
+++ sendText.js
@@ -0,0 +1,24 @@
+// import dotenv
+require('dotenv').config()
+// set up environment variables
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+// require twilio
+const client = require('twilio')(accountSid, authToken);
+
+const recipient = '+18572611633';
+
+// regex to validate phone number
+const validatePhone = (phone) => {
+    const regex = /^\+?(\d{1,3})?[-. ]?(\d{3})[-. ]?(\d{3})[-. ]?(\d{4})$/;
+    return regex.test(phone);
+}
+
+// method to send text message
+const sendText = (to, body) => {
+client.messages
+      .create({body: body, from: '+18155690758', to: to})
+    .then(message => console.log('sent text that says:' + body));
+}
+// send message if phone number is valid 
+validatePhone(recipient) ? sendText(recipient, 'I\'m at GitHub Universe 2022') : console.log('invalid phone number');
